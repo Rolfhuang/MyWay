5aa3c750e817062db2748c45d0e498d6646d3496
rhuang367 <rhuang367@gatech.edu>
2024-04-03 23:13:21 -0400

    first commit

---
+++ src/test/java/org/myway/myway/MyWayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myway.myway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
